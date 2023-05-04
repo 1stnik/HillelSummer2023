9c5aa2a6e2345fdfea990998036a0194d7e7f419
Oleksandr Stepurko <stepurko.alexandr@gmail.com>
2023-05-04 20:06:28 +0200

    Classwork 3 Access Modifiers Species types Java control structure arrays Foreach

---
+++ hillelClassworks/src.com.hillel/lesson_03/hw3/Hw3.java
@@ -0,0 +1,19 @@
+package lesson_03.hw3;
+
+public class Hw3 {
+
+    public void forPrintFrom1to99(){
+
+    }
+
+    public void whileDoPrintFrom1to99(){
+
+    }
+
+     public void doWhilePrintFrom1to99(){
+
+    }
+
+
+}
+
---
+++ hillelClassworks/src.com.hillel/lesson_03/hw3/Main.java
@@ -0,0 +1,14 @@
+package lesson_03.hw3;
+
+public class Main {
+    public static void main(String[] args) {
+        Hw3 hw = new Hw3();
+
+        System.out.println("task 1");
+        hw.whileDoPrintFrom1to99();
+
+        System.out.println("task 2");
+        hw.forPrintFrom1to99();
+
+    }
+}
