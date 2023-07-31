6312b0bc2b3e6f2c8d226e691a4c021a3ba5550b
oleksand.stepurko <stepurko.alexandr@gmail.com>
2023-07-31 21:01:56 +0200

    Lesson #24 Rest update

---
+++ mvn/spark-java/src/main/java/com/sparkjava/Product.java
@@ -0,0 +1,15 @@
+package com.sparkjava;
+
+import java.util.UUID;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class Product {
+    private UUID UUID;
+    private String name;
+    private double cost;
+}
