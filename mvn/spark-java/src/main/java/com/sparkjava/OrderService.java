a8c1ef2194d66923a7c82843b6b825e707357c7b
oleksand.stepurko <stepurko.alexandr@gmail.com>
2023-08-07 20:11:11 +0200

    Lesson #27 reflection

--- mvn/spark-java/src/main/java/com/sparkjava/OrderService.java
+++ mvn/spark-java/src/main/java/com/sparkjava/OrderService.java
@@ -1,5 +1,6 @@
 package com.sparkjava;
 
+import static spark.Spark.delete;
 import static spark.Spark.get;
 import static spark.Spark.post;
 import static spark.Spark.put;
@@ -7,6 +8,7 @@ import static spark.Spark.put;
 import com.google.gson.Gson;
 import java.sql.Timestamp;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
@@ -54,6 +56,32 @@ public class OrderService {
             return new Gson().toJsonTree(order);
         });
 
+
+        // create order
+        post("/orders", (request, response)  -> {
+            response.type("application/json");
+
+            // receive product from UI
+            Product[] products = new Gson().fromJson(request.body(), Product[].class);
+
+            for (Product product : products) {
+                product.setUUID(UUID.randomUUID());
+            }
+
+            // we dont have order yet -> we create order and put product inside
+            Order order = new Order();
+            order.setUuid(UUID.randomUUID());
+            order.setCreateAt(new Timestamp(System.currentTimeMillis()));
+            order.setUpdateAt(new Timestamp(System.currentTimeMillis()));
+            // add product
+            order.getProducts().addAll(Arrays.asList(products));
+            // calculate total cost
+            order.setCost(order.getProducts().stream().mapToDouble(Product::getCost).sum());
+
+            orderList.add(order);
+            return new Gson().toJsonTree(order);
+        });
+
         // update order
         put("/order/:uuid", (request, response)  -> {
             response.type("application/json");
@@ -73,6 +101,18 @@ public class OrderService {
             order.setCost(order.getProducts().stream().mapToDouble(Product::getCost).sum());
 
             return new Gson().toJsonTree(order);
+        });
+
+        delete("/order/:uuidOrder/:uuidProduct", (request, response)  -> {
+            response.type("application/json");
+
+            UUID uuidOrder = UUID.fromString(request.params(":uuidOrder"));
+            UUID uuidProduct = UUID.fromString(request.params(":uuidProduct"));
+
+            // first search order
+            // them search and delete product
+
+            return null;
         });
     }
 }
